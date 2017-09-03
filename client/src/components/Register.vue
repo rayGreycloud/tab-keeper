cfa2b5b5a3f9d1f1d82448318f97e854a97781fc
rayGreycloud <rayGreycloud@gmail.com>
2017-09-03 16:17:12 -0700

    Add register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"
+     />
+     <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+     />
+     <br>
+     <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
